ff6c9399029a3c83f69dd53dd5a91be48d8dfa80
mzkiskw <srminmin1007@gmail.com>
2023-01-31 13:31:54 +0900

    Add Apis test

---
+++ tests/Api/Kintone/ApisTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CybozuHttp\Tests\Api\Kintone;
+
+use PHPUnit\Framework\TestCase;
+use KintoneTestHelper;
+
+use CybozuHttp\Api\KintoneApi;
+
+class ApisTest extends TestCase
+{
+    /**
+     * @var KintoneApi
+     */
+    private $api;
+
+    protected function setup(): void
+    {
+        $this->api = KintoneTestHelper::getKintoneApi();
+    }
+
+    public function testGet(): void {
+        $apis = $this->api->apis()->get();
+        $this->assertTrue(isset($apis['baseUrl']));
+        $this->assertIsArray($apis['apis']);
+    }
+
+}
